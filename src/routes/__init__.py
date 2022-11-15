3b8dbd5b59d5e5d47f2e9f4a84edc5d7362655db
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-11-15 17:11:10 +0100

    new: Example route

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,7 +3,11 @@ src/routes/__init__.py
 
 @author Altair Bueno <altair.bueno@uma.es>
 '''
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from ..beans import get_collection
 
 BaseRouter = APIRouter()
 
+@BaseRouter.get("/example")
+async def example(collection=Depends(get_collection)):
+    return [x async for x in collection.find({})]
